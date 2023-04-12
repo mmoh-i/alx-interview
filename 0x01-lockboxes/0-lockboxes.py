0752649e3f7e039a019e579d06714b555caac7f1
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-12 20:31:55 +0100

    Create 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from typing import List
+
+def canUnlockAll(boxes: List[List[int]]) -> bool:
+    """
+    Determines whether all the boxes in the list can be opened.
+    
+    Args:
+        boxes (List[List[int]]): A list of lists representing the boxes.
+            Each list represents a box and contains integers representing the
+            keys that the box contains. The first box boxes[0] is unlocked.
+    
+    Returns:
+        bool: True if all the boxes can be opened, False otherwise.
+    """
+    num_boxes = len(boxes)
+    unlocked_boxes = [False] * num_boxes
+    unlocked_boxes[0] = True
+
+    # Use a stack to keep track of boxes that we can currently unlock
+    stack = [0]
+
+    while stack:
+        box_num = stack.pop()
+        box = boxes[box_num]
+        for key in box:
+            # Check that the key corresponds to a valid box and that the box hasn't already been unlocked
+            if key < num_boxes and not unlocked_boxes[key]:
+                unlocked_boxes[key] = True
+                stack.append(key)
+
+    return all(unlocked_boxes)
