d7c7a6253521dad5667df374f3cd9ae15190adac
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-15 14:27:16 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,35 +4,29 @@
 
 import sys
 
-# Initialize variables
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
 for line in sys.stdin:
-    # Parse the line
     try:
         parts = line.split()
         ip_address = parts[0]
         status_code = int(parts[8])
         file_size = int(parts[9])
     except:
-        # If the line cannot be parsed, skip it
         continue
 
-    # Update variables
     total_size += file_size
     status_codes[status_code] += 1
     line_count += 1
 
-    # Print statistics after every 10 lines
     if line_count % 10 == 0:
         print("Total file size: File size: {}".format(total_size))
         for code, count in sorted(status_codes.items()):
             if count > 0:
                 print("{}: {}".format(code, count))
 
-# Print final statistics
 print("Total file size: File size: {}".format(total_size))
 for code, count in sorted(status_codes.items()):
     if count > 0:
